322c17601bb015246d70e07cf57532c0436f20bf
Marco Dugatto <marco.dugatto@novasrl.net>
2026-02-11 14:07:26 +0100

    Added rock breaking SFX and particles

--- Scripts/Environment/breakable.gd
+++ Scripts/Environment/breakable.gd
@@ -5,9 +5,16 @@ extends StaticBody2D
 var currentHP: int
 
 @onready var control: Control = $Control
-@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 var player_is_in_range: bool = false
+
+@onready var hit: AudioStreamPlayer = $Audio/Hit
+@onready var destroy: AudioStreamPlayer = $Audio/Destroy
+
+@onready var rock_particles_parent: Node2D = $RockParticlesParent
+@onready var rock_particles = preload("res://Scenes/Particles/rock_particles.tscn")
+var last_rock_particles_node: GPUParticles2D
 
 
 func _ready() -> void:
@@ -20,12 +27,30 @@ func on_interacted() -> void:
 	if not player_is_in_range:
 		return
 	
-	currentHP -= 1
-	if currentHP <= 0:
-		call_deferred("queue_free")
-	
-	gpu_particles_2d.restart()
 	EventBus.emit_signal("breakable_damaged")
+	play_rock_particles()
+	currentHP -= 1
+	last_rock_particles_node
+	if currentHP <= 0:
+		destroy.play()
+		defer_destroy_object()
+	else:
+		hit.play()
+
+
+func play_rock_particles() -> void:
+	last_rock_particles_node = rock_particles.instantiate() as GPUParticles2D
+	rock_particles_parent.add_child(last_rock_particles_node)
+	last_rock_particles_node.restart()
+
+
+func defer_destroy_object() -> void:
+	collision_shape_2d.call_deferred("queue_free")
+	control.hide()
+	
+	await last_rock_particles_node.finished
+	
+	call_deferred("queue_free")
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
