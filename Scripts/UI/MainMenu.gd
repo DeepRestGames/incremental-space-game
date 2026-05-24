078500727b7df94bd0b4a9abd9e6bd1c77075baf
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2026-05-24 18:28:08 +0200

    Added menu to better navigate and debug

---
+++ Scripts/UI/MainMenu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _on_continue_button_pressed() -> void:
+	# Placeholder
+	pass
+
+
+func _on_new_game_button_pressed() -> void:
+	# Start a new game
+	get_tree().change_scene_to_file("res://Scenes/Prototypes/Prototype_MainScene.tscn")
+
+
+func _on_skill_tree_test_button_pressed() -> void:
+	# Test skill tree
+	get_tree().change_scene_to_file("res://Scenes/UI/skill_tree.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	# Quit
+	get_tree().quit()
