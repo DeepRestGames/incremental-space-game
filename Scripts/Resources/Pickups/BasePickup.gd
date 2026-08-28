d3f453bf47d53897e9a117ad92dbbbb3d605db07
enirfo <enrico.fornasa.it@gmail.com>
2026-08-28 17:38:50 +0200

    updated pickup to spawn like particles

--- Scripts/Resources/Pickups/BasePickup.gd
+++ Scripts/Resources/Pickups/BasePickup.gd
@@ -1,8 +1,11 @@
 class_name BasePickup
 extends Area2D
 
+#Graphics
 @onready var animation_player_glow = $AnimationPlayerGlow
 @onready var animation_player_shine_spikes = $AnimationPlayerShineSpikes
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 
 @export_group("Pickup Info")
 @export var pickup_amount: int = 1
@@ -73,15 +76,25 @@ func _on_move_to_player_area_2d_body_exited(body: Node2D) -> void:
 
 func randomize_spawn_direction() -> void:
 	var direction = Vector2(randf_range(-1, 1), randf_range(-1, 1))
-	var spawn_linear_velocity = randi_range(120, 200)
+	var spawn_linear_velocity = randi_range(120, 300)
 	var spawn_angular_velocity = randf_range(2, 5)
 	var spawn_duration = randf_range(.5, 1)
 	
-	var tween_y = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+	#sprite starting y position (= starting fake z)
+	var starting_z = randf_range(-10, -50)
+	var z_height = randf_range(50, 100)
+	sprite_2d.position.y = starting_z
+	
+	var tween_y = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween_y.tween_property(self, "position:y", position.y + (direction.y * spawn_linear_velocity), spawn_duration)
+	
+	#fake z movement
+	var tween_z = get_tree().create_tween().set_ease(Tween.EASE_OUT)
+	tween_z.tween_property(sprite_2d, "position:y", starting_z - z_height, spawn_duration/4).set_trans(Tween.TRANS_CUBIC)
+	tween_z.tween_property(sprite_2d, "position:y", 0, spawn_duration/4*3).set_trans(Tween.TRANS_BOUNCE)
 	
 	var tween_x = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	tween_x.tween_property(self, "position:x", position.x + (direction.x * spawn_linear_velocity), spawn_duration)
 	
 	var tween_rotation = get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween_rotation.tween_property(self, "rotation", rotation + spawn_angular_velocity, spawn_duration)
+	tween_rotation.tween_property(sprite_2d, "rotation", rotation + spawn_angular_velocity, spawn_duration)
